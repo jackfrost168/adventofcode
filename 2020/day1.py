477d4116936538bbb1d58f6fbe2881501a8cc2cc
jackfrost168 <jack_frost118@163.com>
2020-12-15 23:30:49 +0800

    Initial commit

---
+++ 2020/day1.py
@@ -0,0 +1,37 @@
+def sum2(arr):
+    ans = 0
+    for a in arr:
+        if 2020 - a in arr:
+            ans = a * (2020 - a)
+            return ans
+
+
+def sum3(arr):
+    ans = 0
+    arr = sorted(arr)
+    for i in range(len(arr)):
+        j = i + 1
+        k = len(arr) - 1
+        while j < k:
+            if arr[i] + arr[j] + arr[k] > 2020:
+                k = k - 1
+            elif arr[i] + arr[j] + arr[k] < 2020:
+                j = j + 1
+            else:
+                ans = arr[i] * arr[j] * arr[k]
+                return ans
+
+
+def main():
+    f = open("input1.txt", "r")  # open file
+    lines = f.readlines()  # read line, lines stores the txt file
+    input = []
+    for line in lines:
+        line = line.strip('\n')  # take away '\n'
+        input.append(int(line))
+    ans1 = sum2(input)
+    print("part1:", ans1)
+    ans2 = sum3(input)
+    print("part2:", ans2)
+
+main()
---
+++ 2020/day2.py
@@ -0,0 +1,48 @@
+def part1(input):
+    ans = 0
+    for line in input:
+        lower_bound = line[0]
+        upper_bound = line[1]
+        target = line[2]
+        string = line[3]
+        count = 0
+        for word in string:
+            if word == target:
+                count = count + 1
+        if count >= lower_bound and count <= upper_bound:
+            ans = ans + 1
+    return ans
+
+def part2(input):
+    ans = 0
+    for line in input:
+        pos1 = line[0]
+        pos2 = line[1]
+        target = line[2]
+        string = line[3]
+        if (string[pos1-1] == target and string[pos2-1] != target) or\
+            (string[pos1-1] != target and string[pos2-1] == target):
+            ans = ans + 1
+    return ans
+
+def main():
+    f = open("input2.txt", "r")  # open file
+    lines = f.readlines()  # read line, lines stores the txt file
+    input = []  # [2, 9, 'g', 'jpxcgggzgsgngrhght']
+    for line in lines:
+        line = line.strip('\n')
+        line = line.split(" ")  # take away '\n'
+        tmp = []
+        r = line[0].split('-')
+        tmp.append(int(r[0]))
+        tmp.append((int(r[1])))
+        tmp.append(line[1][0]) #single word
+        tmp.append(line[2]) #string
+        input.append(tmp)
+
+    ans1 = part1(input)
+    print("part1:", ans1)
+    ans2 = part2(input)
+    print("part2:", ans2)
+
+main()
