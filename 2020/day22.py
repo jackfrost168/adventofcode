6326625df848200bbcd476ca50aecd92e5320c44
jackfrost168 <jack_frost118@163.com>
2020-12-22 15:53:05 +0800

    code more readable

--- 2020/day22.py
+++ 2020/day22.py
@@ -33,29 +33,29 @@ def game(player1, player2):
 
         card_1 = player1.pop(0)
         card_2 = player2.pop(0)
-        winner = 0
+        final_winner = 0
         if card_1 <= len(player1) and card_2 <= len(player2):
-            winner = game(player1[0: card_1], player2[0: card_2])
+            final_winner = game(player1[0: card_1], player2[0: card_2])
         else:
             if card_1 > card_2:
-                winner = 1
+                final_winner = 1
             elif card_1 < card_2:
-                winner = 2
+                final_winner = 2
 
-        if winner == 1:
+        if final_winner == 1:
             player1.append(card_1)
             player1.append(card_2)
-        elif winner == 2:
+        elif final_winner == 2:
             player2.append(card_2)
             player2.append(card_1)
         if len(player1) == 50 or len(player2) == 50:
             if player1:
-                winner = player1[:]
+                final_winner = player1[:]
             else:
-                winner = player2[:]
+                final_winner = player2[:]
             score = 0
-            weight = len(winner)
-            for value in winner:
+            weight = len(final_winner)
+            for value in final_winner:
                 score += value * weight
                 weight = weight - 1
             print('part2:', score)
