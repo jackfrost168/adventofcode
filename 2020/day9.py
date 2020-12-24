08a304897a2989ae498c1719a6eb3e492bd0044a
jackfrost168 <jack_frost118@163.com>
2020-12-24 22:50:34 +0800

    day 9

--- 2020/day9.py
+++ 2020/day9.py
@@ -1,33 +1,33 @@
-def part1(input):
-    for i in range(25, len(input)):
-        tmp = input[i-25: i]
+def part1(preamble):
+    for i in range(25, len(preamble)):
+        tmp = preamble[i - 25: i]
         cur_num = False
         for t in tmp:
-            if input[i] - t in tmp and input[i] - t != t:
+            if preamble[i] - t in tmp and preamble[i] - t != t:
                 cur_num = True
                 break
 
         if cur_num == False:
-            return input[i]
+            return preamble[i]
 
 
-def part2(input, target):
-    for i in range(len(input)):
-        tmp = input[i]
-        for j in range(i+1, len(input)):
-            tmp = tmp + input[j]
+def part2(preamble, target):
+    for i in range(len(preamble)):
+        tmp = preamble[i]
+        for j in range(i+1, len(preamble)):
+            tmp = tmp + preamble[j]
             if tmp == target:
-                return max(input[i:j + 1])+min(input[i:j + 1])
+                return max(preamble[i:j + 1]) + min(preamble[i:j + 1])
 
 
 def main():
-    with open("input/input9.txt", "r") as f: # open file
-        lines = f.readlines()  # read line, lines stores the txt file
-        input = [int(line.strip()) for line in lines]
+    with open("input/input9.txt", "r") as f:
+        lines = f.readlines()
+        preamble = [int(line.strip()) for line in lines]
 
-    ans1 = part1(input)
+    ans1 = part1(preamble)
     print('part1:', ans1)
-    ans2 = part2(input, ans1)
+    ans2 = part2(preamble, ans1)
     print('part2:', ans2)
 
 
