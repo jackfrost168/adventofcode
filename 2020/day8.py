1103977b63efc5119ef10323698419970749404a
jackfrost168 <jack_frost118@163.com>
2020-12-26 13:52:27 +0800

    fixed day 8

--- 2020/day8.py
+++ 2020/day8.py
@@ -3,7 +3,7 @@ import copy
 def part1(boot_code):
     accumulator = 0
     i = 0
-    while i < len(boot_code.keys()):
+    while i < len(boot_code):
         instruction = boot_code[i][0]
         argument = boot_code[i][1]
         if boot_code[i][2] == 1:
@@ -23,7 +23,7 @@ def part1(boot_code):
 
 
 def part2(boot_code):
-    for key in boot_code.keys():
+    for key in range(len(boot_code)):
         cur_boot_code = copy.deepcopy(boot_code)
         if boot_code[key][0] == 'nop':
             cur_boot_code[key][0] = 'jmp'
@@ -35,7 +35,7 @@ def part2(boot_code):
         accumulator = 0
         i = 0
         isloop = True
-        while i < len(cur_boot_code.keys()):
+        while i < len(cur_boot_code):
             instruction = cur_boot_code[i][0]
             argument = cur_boot_code[i][1]
             if cur_boot_code[i][2] == 1:
@@ -51,19 +51,19 @@ def part2(boot_code):
                 else:
                     cur_boot_code[i][2] = 1
                     i = i + argument
-        if i >= len(cur_boot_code.keys()):
+        if i >= len(cur_boot_code):
             isloop = False
-        if isloop == False:
+        if isloop is False:
             return accumulator
 
 
 def main():
     with open("input/input8.txt", "r") as f:
         lines = f.readlines()
-        boot_code = {}
-        for id, line in enumerate(lines):
+        boot_code = []
+        for line in lines:
             line = line.strip('\n').split(' ')
-            boot_code[id] = [line[0], int(line[1]), 0]
+            boot_code.append([line[0], int(line[1]), 0])  # [jmp, 323, 0]
 
     boot_code1 = copy.deepcopy(boot_code)
     ans1 = part1(boot_code1)
