bc3d83209d0e174da52b9decfb0a1947aa46b084
jackfrost168 <jack_frost118@163.com>
2020-12-29 13:37:34 +0800

    2018 day 1

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,28 +1,27 @@
-def solution1(input):
+def solution1(masses):
     ans = 0
-    for num in input:
+    for num in masses:
         ans = ans + num // 3 - 2
     return ans
 
 
-def solution2(input):
+def solution2(masses):
     ans = 0
-    for num in input:
-        while num > 5: #num must greater than 0
+    for num in masses:
+        while num > 5:  # num must greater than 0
             num = num // 3 - 2
             ans = ans + num
     return ans
 
 
 def main():
-    with open("input/input1.txt", "r") as f: # open file
-        f = f.readlines()  # read line, lines stores the txt file
-        input = [int(line.strip()) for line in f]
+    with open("input/input1.txt", "r") as f:  # open file
+        masses = [int(line.strip()) for line in f]
 
-    ans1 = solution1(input)
-    print("part1:", ans1)
-    ans2 = solution2(input)
-    print("part2:", ans2)
+    ans1 = solution1(masses)
+    print("part 1:", ans1)
+    ans2 = solution2(masses)
+    print("part 2:", ans2)
 
 
 main()
