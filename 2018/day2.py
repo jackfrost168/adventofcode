955948612cc78fab70e2079c3a00e25267a73d66
jackfrost168 <jack_frost118@163.com>
2020-12-29 13:40:49 +0800

    2018 day 2

--- 2018/day2.py
+++ 2018/day2.py
@@ -1,7 +1,6 @@
-def part1(input):
-    two = 0
-    three = 0
-    for string in input:
+def part1(candidates):
+    two, three = 0, 0
+    for string in candidates:
         appeared = {}
         have_two = False
         have_three = False
@@ -15,39 +14,38 @@ def part1(input):
                 have_two = True
             if appeared[key] == 3:
                 have_three = True
-        if have_two == True:
+        if have_two:
             two = two + 1
-        if have_three == True:
+        if have_three:
             three = three + 1
-    return two*three
+    return two * three
 
 
-def part2(input):
-    for i in range(len(input)):
-        string1 = input[i]
-        for j in range(i+1, len(input)):
-            string2 = input[j]
+def part2(candidates):
+    for i in range(len(candidates)):
+        string1 = candidates[i]
+        for j in range(i+1, len(candidates)):
+            string2 = candidates[j]
             diff = 0
-            for k in range(len(input[0])):
+            for k in range(len(candidates[0])):
                 if string1[k] != string2[k]:
                     diff += 1
             if diff == 1:
                 ans = ''
-                for m in range(len(input[0])):
+                for m in range(len(candidates[0])):
                     if string1[m] == string2[m]:
                         ans = ans + string1[m]
                 return ans
 
 
 def main():
-    with open("input/input2.txt", "r") as f: # open file
-        lines = f.readlines()  # read line, lines stores the txt file
-        input = [line.strip() for line in lines]
+    with open("input/input2.txt", "r") as f:  # open file
+        candidates = [line.strip() for line in f]
 
-    ans1 = part1(input)
-    print('part1:', ans1)
-    ans2 = part2(input)
-    print('part2:', ans2)
+    ans1 = part1(candidates)
+    print('part 1:', ans1)
+    ans2 = part2(candidates)
+    print('part 2:', ans2)
 
 
 main()
