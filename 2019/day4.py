46a9a7c21272c65f42bfc2ba7078a595f88c6df3
jackfrost168 <jack_frost118@163.com>
2020-12-29 13:41:50 +0800

    2019 day 4

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,13 +1,13 @@
 def part1(lower_bound, upper_bound):
     ans = 0
     for i in range(lower_bound, upper_bound + 1):
-        string = str(i)
+        digits_string = str(i)
         double_flag = 0
         increase_flag = 1
         for j in range(1, 6):
-            if int(string[j]) == int(string[j-1]):
+            if int(digits_string[j]) == int(digits_string[j-1]):
                 double_flag = 1
-            if int(string[j]) < int(string[j-1]):
+            if int(digits_string[j]) < int(digits_string[j-1]):
                 increase_flag = 0
                 break
         if double_flag == 1 and increase_flag == 1:
@@ -18,21 +18,21 @@ def part1(lower_bound, upper_bound):
 def part2(lower_bound, upper_bound):
     ans = 0
     for i in range(lower_bound, upper_bound + 1):
-        string = str(i)
+        digits_string = str(i)
         double_flag = 0
         increase_flag = 1
-        num_dict = {}
+        digit_dict = {}
         for j in range(1, 6):
-            if int(string[j]) == int(string[j-1]):
-                if int(string[j]) not in num_dict.keys():
-                    num_dict[int(string[j])] = 2
+            if int(digits_string[j]) == int(digits_string[j-1]):
+                if int(digits_string[j]) not in digit_dict.keys():
+                    digit_dict[int(digits_string[j])] = 2
                 else:
-                    num_dict[int(string[j])] += 1
-            if int(string[j]) < int(string[j-1]):
+                    digit_dict[int(digits_string[j])] += 1
+            if int(digits_string[j]) < int(digits_string[j-1]):
                 increase_flag = 0
                 break
-        for key in num_dict.keys():
-            if num_dict[key] == 2:
+        for key in digit_dict.keys():
+            if digit_dict[key] == 2:
                 double_flag = 1
         if double_flag == 1 and increase_flag == 1:
             ans = ans + 1
@@ -41,15 +41,15 @@ def part2(lower_bound, upper_bound):
 
 def main():
     with open('input/input4.txt', 'r') as f:
-        f = f.read().strip().split("-")  #f = "264793-803935".split("-")
+        f = f.read().strip().split("-")  # f = "264793-803935".split("-")
 
     lower_bound = int(f[0])
     upper_bound = int(f[1])
 
     ans1 = part1(lower_bound, upper_bound)
-    print("part1:", ans1)
+    print("part 1:", ans1)
     ans2 = part2(lower_bound, upper_bound)
-    print("part2:", ans2)
+    print("part 2:", ans2)
 
 
 main()
