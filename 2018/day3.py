1220596a7eba562626536d328cb4a365b3864a62
jackfrost168 <jack_frost118@163.com>
2020-12-29 13:41:27 +0800

    2018 day 3

--- 2018/day3.py
+++ 2018/day3.py
@@ -1,11 +1,9 @@
-def areas(input):
+def areas(claims):
     fabric = {}
-    for key in input.keys():
-        claim = input[key]
-        x = claim[0]
-        y = claim[1]
-        width = claim[2]
-        height = claim[3]
+    for key in claims.keys():
+        claim = claims[key]
+        x, y = claim[0], claim[1]
+        width, height = claim[2], claim[3]
         for i in range(height):
             for j in range(width):
                 if (x+i, y+j) not in fabric.keys():
@@ -23,47 +21,39 @@ def part1(fabric):
     return ans
 
 
-def part2(input, fabric):
-    for key in input.keys():
-        claim = input[key]
-        x = claim[0]
-        y = claim[1]
-        width = claim[2]
-        height = claim[3]
+def part2(claims, fabric):
+    for key in claims.keys():
+        claim = claims[key]
+        x, y = claim[0], claim[1]
+        width, height = claim[2], claim[3]
         overlab = False
         for i in range(height):
             for j in range(width):
                 if fabric[(x + i, y + j)] >= 2:
                     overlab = True
                     break
-            if overlab == True:
+            if overlab:
                 break
-        if overlab == False:
+        if overlab is False:
             return key
 
 
 def main():
     with open('input/input3.txt', 'r') as f:
-        lines = f.readlines()
-        input = {}
-        for line in lines:
-            line = line.strip('\n')
-            line = line.split(' ')
-            id = int(line[0][1:])
-            pos = str(line[2]).replace(':', '')
-            pos = pos.split(',')
-            x = int(pos[1])
-            y = int(pos[0])
-            area = str(line[3]).split('x')
-            width = int(area[0])
-            height = int(area[1])
-            input[id] = (x, y, width, height)
+        claims = {}  # {id: (576, 16, 17, 14)}
+        for line in f:
+            line = line.strip().replace('#', '').replace('@ ', '').replace(':', '')\
+                .replace(',', ' ').replace('x', ' ').split(' ')
+            ID = int(line[0])
+            x, y = int(line[2]), int(line[1])
+            width, height = int(line[3]), int(line[4])
+            claims[ID] = (x, y, width, height)
 
-    fabric = areas(input)
+    fabric = areas(claims)
     ans1 = part1(fabric)
-    print('part1:', ans1)
-    ans2 = part2(input, fabric)
-    print('part2:', ans2)
+    print('part 1:', ans1)
+    ans2 = part2(claims, fabric)
+    print('part 2:', ans2)
 
 
 main()
