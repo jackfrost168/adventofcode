207a5d457f363154b21e08594d4aeb1ec0956043
jackfrost168 <jack_frost118@163.com>
2020-12-30 21:32:08 +0800

    2020 day 23

--- 2020/day23.py
+++ 2020/day23.py
@@ -95,3 +95,33 @@ def main():
 
 
 main()
+
+
+# class Node:
+#     pass
+#
+# vals = [int(x) for x in list('942387615')] + list(range(10,1000000+1))
+# nodes = [Node() for _ in vals]
+#
+# for node, other, val in zip(nodes, nodes[1:] + nodes, vals):
+#     node.right = other
+#     node.value = val
+# lookup = {node.value: node for node in nodes}
+#
+# cur = nodes[0]
+# for _ in range(10000000):
+#     three = cur.right, cur.right.right, cur.right.right.right
+#     cur.right = three[-1].right
+#     n = cur.value - 1
+#     if n == 0:
+#         n = 1000000
+#     while n not in range(1, 1000000+1) or any(node.value == n for node in three):
+#         n -= 1
+#         if n == 0:
+#             n = 1000000
+#     dest = lookup[n]
+#     three[2].right = dest.right
+#     dest.right = three[0]
+#     cur = cur.right
+#
+# print(lookup[1].right.value * lookup[1].right.right.value)
