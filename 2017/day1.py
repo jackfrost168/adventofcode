d2cea4bdd254af2d68978cdcdc15a219a5402e6b
jackfrost168 <jack_frost118@163.com>
2021-01-06 23:16:17 +0800

    2017 day 1

---
+++ 2017/day1.py
@@ -0,0 +1,13 @@
+with open('input/input1.txt', 'r') as f:
+    digits = f.read().strip()
+
+captcha = 0
+for i in range(len(digits)):
+    if i == len(digits) - 1:
+        if digits[i] == digits[0]:
+            captcha += int(digits[i])
+    else:
+        if digits[i] == digits[i+1]:
+            captcha += int(digits[i])
+
+print(captcha)
