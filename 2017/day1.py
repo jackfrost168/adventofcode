b0504bba2e0889e075a727234aa42ecb18c37e9f
jackfrost168 <jack_frost118@163.com>
2021-01-07 20:59:45 +0800

    2017 day 1

--- 2017/day1.py
+++ 2017/day1.py
@@ -1,15 +1,31 @@
-with open('input/input1.txt', 'r') as f:
-    digits = f.read().strip()
-
-captcha = 0
-for i in range(len(digits)):
-    if i == len(digits) - 1:
-        if digits[i] == digits[0]:
-            captcha += int(digits[i])
-    else:
-        if digits[i] == digits[i+1]:
+def part1(digits):
+    captcha = 0
+    for i in range(len(digits)):
+        match_pos = (i + 1) % len(digits)
+        if digits[i] == digits[match_pos]:
             captcha += int(digits[i])
 
-print(captcha)
+    return captcha
 
 
+def part2(digits):
+    captcha = 0
+    for i in range(len(digits)):
+        match_pos = (i + len(digits)//2) % len(digits)
+        if digits[i] == digits[match_pos]:
+            captcha += int(digits[i])
+
+    return captcha
+
+
+def main():
+    with open('input/input1.txt', 'r') as f:
+        digits = f.read().strip()
+
+    ans1 = part1(digits)
+    print('part 1:', ans1)
+    ans2 = part2(digits)
+    print('part 2:', ans2)
+
+
+main()
