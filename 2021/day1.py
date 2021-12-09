50da6c2ee6501c84e9df0294508aa00882423943
HouYu <jack_frost118@163.com>
2021-12-10 00:42:14 +0900

    2021 day1/2

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,29 +1,31 @@
 def part1(arr):
-    count = 0
-    for i in range(1, len(arr)):
-        if arr[i] > arr[i-1]:
-            count += 1
+    # count = 0
+    # for i in range(1, len(arr)):
+    #     if arr[i] > arr[i-1]:
+    #         count += 1
+
+    count = sum(b > a for a, b in zip(arr, arr[1:]))
 
     return count
 
 
 def part2(arr):
-    count = 0
-    for i in range(1, len(arr)-2):
-        if arr[i] + arr[i+1] + arr[i+2] > arr[i-1] + arr[i] + arr[i+1]:
-            count += 1
+    # count = 0
+    # for i in range(1, len(arr)-2):
+    #     if arr[i] + arr[i+1] + arr[i+2] > arr[i-1] + arr[i] + arr[i+1]:
+    #         count += 1
+
+    count = sum(b > a for a, b in zip(arr, arr[3:]))
+
     return count
 
 
 def main():
     with open("input/input1.txt", "r") as f:  # open file
-        f = f.readlines()  # read line, lines stores the txt file
-        report = [int(line.strip()) for line in f]
+        report = [int(line.strip()) for line in f.read().splitlines()]
 
-    ans1 = part1(report)
-    print("part 1:", ans1)
-    ans2 = part2(report)
-    print("part 2:", ans2)
+    print("part 1:", part1(report))
+    print("part 2:", part2(report))
 
 
 main()
--- 2021/day2.py
+++ 2021/day2.py
@@ -30,13 +30,10 @@ def part2(commands):
 
 def main():
     with open("input/input2.txt", "r") as f:  # open file
-        f = f.readlines()  # read line, lines stores the txt file
-        commands = [line.strip().split() for line in f]
+        commands = [line.strip().split() for line in f.read().splitlines()]
 
-    ans1 = part1(commands)
-    print("part 1:", ans1)
-    ans2 = part2(commands)
-    print("part 2:", ans2)
+    print("part 1:", part1(commands))
+    print("part 2:", part2(commands))
 
 
 main()
