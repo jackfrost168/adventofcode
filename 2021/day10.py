6f3d3b690bf7893c90af9462f22f48a7368c0b79
HouYu <jack_frost118@163.com>
2021-12-10 14:37:58 +0900

    2021 day10

---
+++ 2021/day10.py
@@ -0,0 +1,88 @@
+def part1(subsystem):
+    score = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    errors = 0
+    for line in subsystem:
+        stack = []
+        for s in line:
+            if s in '([{<':
+                stack.append(s)
+            else:
+                brackt = stack[-1] + s
+                if brackt in ['()', '[]', '{}', '<>']:
+                    stack.pop()
+                else:
+                    errors += score[s]
+                    break
+
+    return errors
+
+
+def part2(subsystem):
+    score = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    errors = 0
+    corrupted = subsystem[:]
+    for line in subsystem:
+        stack = []
+        for s in line:
+            if s in '([{<':
+                stack.append(s)
+            else:
+                brackt = stack[-1] + s
+                if brackt in ['()', '[]', '{}', '<>']:
+                    stack.pop()
+                else:
+                    errors += score[s]
+                    corrupted.remove(line)
+                    break
+
+    print(corrupted)
+    stacks = []
+    for line in corrupted:
+        stack = []
+        for s in line:
+            if s in '([{<':
+                stack.append(s)
+            else:
+                stack.pop()
+        stacks.append(stack)
+
+    print(stacks)
+    completes = []
+    for stack in stacks:
+        complete = ''
+        for s in stack:
+            if s == '(': complete = ')' + complete
+            elif s == '[': complete = ']' + complete
+            elif s == '{': complete = '}' + complete
+            elif s == '<': complete = '>' + complete
+        completes.append(complete)
+
+    print(completes)
+
+    table = {')': 1, ']': 2, '}': 3, '>': 4}
+    scores = []
+    for complete in completes:
+        score = 0
+        for s in complete:
+            score = score * 5 + table[s]
+
+        scores.append(score)
+    print(scores)
+
+    scores = sorted(scores)
+
+    return scores[len(scores)//2]
+
+
+
+def main():
+    with open("input/input10.txt", "r") as f:  # open file
+        f = f.readlines()  # read line, lines stores the txt file
+        subsystem = [line.strip() for line in f]
+        print(subsystem)
+
+    print("part 1:", part1(subsystem[:]))
+    print("part 2:", part2(subsystem[:]))
+
+
+main()
