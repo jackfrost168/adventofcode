12ee51ae33cf0333047b2fd060684ab238cdaa33
HouYu <jack_frost118@163.com>
2021-12-24 00:46:02 +0900

    2021 day13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,70 +1,61 @@
-from collections import defaultdict
-
-
 def part1(paper, folds):
-    for axis, pos in folds:
-        new_paper = defaultdict()
-        if axis == 'x':
-            for x, y in paper.keys():
-                if x <= pos:
-                    new_paper[(x, y)] = 1
-                elif x > pos:
-                    if (2*pos - x, y) not in new_paper.keys():
-                        new_paper[(2*pos-x, y)] = 1
-        elif axis == 'y':
-            for x, y in paper.keys():
-                if y <= pos:
-                    new_paper[(x, y)] = 1
-                elif y > pos:
-                    if (x, 2*pos-y) not in new_paper.keys():
-                        new_paper[(x, 2*pos-y)] = 1
-        paper = new_paper.copy()
-        print('length:', len(paper.keys()))
-    return len(paper.keys())
+    axis, pos = folds[0]
+    new_paper = set()
+    if axis == 'x':
+        for x, y in paper:
+            if x <= pos:
+                new_paper.add((x, y))
+            elif x > pos:
+                if (2*pos - x, y) not in new_paper:
+                    new_paper.add((2*pos-x, y))
+    elif axis == 'y':
+        for x, y in paper:
+            if y <= pos:
+                new_paper.add((x, y))
+            elif y > pos:
+                if (x, 2*pos-y) not in new_paper:
+                    new_paper.add((x, 2*pos-y))
+    paper = new_paper.copy()
+
+    return len(paper)
 
 
 def part2(paper, folds):
     for axis, pos in folds:
-        new_paper = defaultdict()
+        new_paper = set()
         if axis == 'x':
-            for x, y in paper.keys():
+            for x, y in paper:
                 if x <= pos:
-                    new_paper[(x, y)] = 1
+                    new_paper.add((x, y))
                 elif x > pos:
-                    if (2*pos - x, y) not in new_paper.keys():
-                        new_paper[(2*pos-x, y)] = 1
+                    if (2*pos - x, y) not in new_paper:
+                        new_paper.add((2*pos-x, y))
         elif axis == 'y':
-            for x, y in paper.keys():
+            for x, y in paper:
                 if y <= pos:
-                    new_paper[(x, y)] = 1
+                    new_paper.add((x, y))
                 elif y > pos:
-                    if (x, 2*pos-y) not in new_paper.keys():
-                        new_paper[(x, 2*pos-y)] = 1
+                    if (x, 2*pos-y) not in new_paper:
+                        new_paper.add((x, 2*pos-y))
         paper = new_paper.copy()
-        print('length:', len(paper.keys()))
 
     max_x, max_y = 0, 0
-    for x, y in paper.keys():
-        if x > max_x:
-            max_x = x
-        if y > max_y:
-            max_y = y
+    for x, y in paper:
+        if x > max_x: max_x = x
+        if y > max_y: max_y = y
 
     for i in range(0, max_y + 1):
         line = ''
         for j in range(0, max_x + 1):
-            if ((j, i) not in paper.keys()):
+            if ((j, i) not in paper):
                 line = line + ' '
             else:
                 line = line + '#'
         print(line)
 
 
-
-
-
 def main():
-    paper = defaultdict(int)
+    paper = set()
     folds = []
     part = 1
     with open("input/input13.txt", "r") as f:  # open file
@@ -75,14 +66,11 @@ def main():
 
             if part == 1:
                 line = line.split(',')
-                paper[(int(line[0]), int(line[1]))] = 1
+                paper.add((int(line[0]), int(line[1])))
             elif part == 2:
                 line = line.split()
                 axis = line[2].split('=')
                 folds.append((axis[0], int(axis[1])))
-
-    print(paper)
-    print(folds)
 
     print("part 1:", part1(paper.copy(), folds[:]))
     print("part 2:")
