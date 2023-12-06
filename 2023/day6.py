16fce65b385a4e447c66321aa13202d72303a993
HouYu <houyu@yonsei.ac.kr>
2023-12-06 16:59:06 +0900

    day6

--- 2023/day6.py
+++ 2023/day6.py
@@ -1,3 +1,5 @@
+import math
+
 def part1(input):
     times, distances = input[0], input[1]
     num_beats = []
@@ -17,19 +19,30 @@ def part1(input):
 
 
 def part2(input):
-    time, distance = '', ''
-    for (t, d) in zip(input[0], input[1]):
-        time = time + str(t)
-        distance = distance + str(d)
-    time, distance = int(time), int(distance)
+    time = int(''.join([str(t) for t in input[0]]))
+    distance = int(''.join([str(d) for d in input[1]]))
 
-    beat = 0
-    for t in range(time):
-        record_dis = t * (time - t)
-        if record_dis > distance:
-            beat += 1
+    a, b, c = 1, -1*time, distance
+    x1 = (-b - math.sqrt(-4 * a * c + b ** 2)) / (2 * a)
+    x2 = (-b + math.sqrt(-4 * a * c + b ** 2)) / (2 * a)
 
-    return beat
+    return abs(int(x1-x2)) + 1
+
+
+# def part2(input):
+#     time, distance = '', ''
+#     for (t, d) in zip(input[0], input[1]):
+#         time = time + str(t)
+#         distance = distance + str(d)
+#     time, distance = int(time), int(distance)
+#
+#     beat = 0
+#     for t in range(time):
+#         record_dis = t * (time - t)
+#         if record_dis > distance:
+#             beat += 1
+#
+#     return beat
 
 
 def main():
